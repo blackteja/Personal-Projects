56a480b28b4e7a2ccbf0bd9d2fd42ac3fe2813a3
Charlie Choi <blackteja1@gmail.com>
2026-06-22 14:41:12 +0900

    홈페이지 제작 프로젝트에 기본 HTML 뼈대만 생성

--- Homepage/index.php
+++ Homepage/index.php
@@ -1,5 +1,18 @@
 <?php
   /*
    * CI Company Homepage Project
+   * 회사 홈페이지를 만들면서 이것저것 기술들을 적용해 본다.
+   * 작성일은 2026년 6월 22일 월요일 14시 36분으로 기본적인 HTML 구문만 넣어놓는다.
   */
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CI Company Homepage</title>
+</head>
+<body>
+  <h1> 홈페이지 제작이 위대한 한걸음이 될 수 있도록 힘내자! </h1>  
+</body>
+</html>
