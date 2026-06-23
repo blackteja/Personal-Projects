cc14d52231f0ae5073ffce31f44366cd95ebb0e3
Charlie Choi <blackteja1@gmail.com>
2026-06-23 11:10:45 +0900

    홈페이지 제작 프로젝트에 기본 CSS 폴더 및 main.css 파일 생성 / 기본 CSS 초기화 및 index.php에 CSS 링크 HTML 코드에 추가

--- Homepage/index.php
+++ Homepage/index.php
@@ -10,6 +10,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/main.css">
   <title>CI Company Homepage</title>
 </head>
 <body>
