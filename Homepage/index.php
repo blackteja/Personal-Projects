3df8587f8e25976e4f77978acd27affa54eb7f33
Charlie Choi <blackteja1@gmail.com>
2026-07-04 01:13:06 +0900

    홈페이지 index.php에 내비게이션과 메뉴를 조금 추가 / 이제 본격적으로 작업해야 될 것 같음

--- Homepage/index.php
+++ Homepage/index.php
@@ -21,11 +21,13 @@
       <h2> Navigaion 영역은 향후 태그를 확인해서 개편 </h2>
       <table>
         <thead>
+          <th>Main</th>
           <th>About</th>
           <th>Portfolio</th>
           <th>Contact</th>
         </thead>
         <tbody>
+          <td>내용작성 예정</td>
           <td>내용작성 예정</td>
           <td>내용작성 예정</td>
           <td>내용작성 예정</td>
@@ -38,6 +40,7 @@
     </navigation>
   </header>
   <h1> 홈페이지 제작이 위대한 한걸음이 될 수 있도록 힘내자! </h1>  
+  <h2>Main Page에 대한 부분은 뭘로 작성하면 좋을까?</h2>
   <footer>
     <h2> Footer 영역 </h2>
     <h3>Help : <a href="mailto:charlie@cicompany.co.kr">charlie@cicompany.co.kr</a></h3>
