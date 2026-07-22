4ce4e0a7a5d70e237c26a487e629fb4f9545b02f
Charlie Choi <blackteja1@gmail.com>
2026-07-23 07:41:26 +0900

    블로그 프로젝트 index.php에 기본 html 태그 추가

--- Blog/index.php
+++ Blog/index.php
@@ -1,5 +1,19 @@
 <?php
   /*
    * Personal Blog Project
+   * Blog 프로젝트는 기본적으로 글쓰기와 View 페이지를 기본으로 만들고 추후 기능을 추가
   */
 ?>
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" contetnt="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <title>Personal Blog Project</title>
+  </head>
+  <body>
+    <!-- 기본만 작성해 놓는다 -->
+    <h1> Personal Blog Project</h1>
+  </body>
+</html>
