2d376576886ceeb0f1ccf275fd2dfcf5261f81f9
Charlie Choi <blackteja1@gmail.com>
2026-07-25 10:03:49 +0900

    홈페이지 index.php에 jquery 관련 스크립트 로딩 추가

--- Homepage/index.php
+++ Homepage/index.php
@@ -11,6 +11,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="css/main.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <title>CI Company Homepage</title>
 </head>
 <body>
