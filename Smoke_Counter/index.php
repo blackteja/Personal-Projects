93333da79179d30fadf4ad519ec683b1fb62fcd5
Charlie Choi <blackteja1@gmail.com>
2026-08-24 01:14:51 +0900

    Smoke_Counter index.php에 기본 html 태그 추가

--- Smoke_Counter/index.php
+++ Smoke_Counter/index.php
@@ -3,3 +3,12 @@
    * 개인 프로젝트에서 담배피는 횟수를 기록하는 스모크 카운터 기본 index.php
   */
 ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Smoke Counter</title>
+  </head>
+  <body>
+    <h1> Smoke Counter Main </h1>
+  </body>
+</html>
