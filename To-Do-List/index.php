2bd81b3ace53bba194939d32163d4ea62c66fc53
Charlie Choi <blackteja1@gmail.com>
2026-09-17 00:06:07 +0900

    To-Do-List Project index.php 파일에 h4 태그 추가

--- To-Do-List/index.php
+++ To-Do-List/index.php
@@ -12,5 +12,6 @@
     <h1> To-Do-List Project</h1>
     <h2> To-Do-List는 간단히 JS와 PHP를 혼합해 제작</h2>
     <h3> Web version을 먼저 제작 후 윈도우 프로그램으로도 연동하도록 만들자</h3>
+    <h4> 간단히라도 기획서를 만들자 </h4>
   </body>
 </html>
